9e7e7da6c90d74f0473850f14dc39a3e2cebed2e
jane muthoni <muchirijane@outlook.com>
2020-09-05 22:51:57 +0300

    react jsx basic syntax

---
+++ index.js
@@ -0,0 +1,20 @@
+//   React Js is an open source view library created and maintained by Facebook.
+//  It's a great tool to render the User Interface (UI) of modern web applications.
+// React uses a syntax extension of Javascript called JSX that allows you to write HTML directly with Javascript.
+
+const JSX = (
+  <div>
+    {" "}
+    <h1> Hello JSX </h1>{" "}
+  </div>
+);
+
+// 2 add comments in react js
+const JSX = (
+  <div>
+    {" "}
+    {/**Valid comment */}
+    <h1> This is a block of Jsx </h1>
+    <p> Here 's a subtitle </p>
+  </div>
+);
