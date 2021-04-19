021314566ab573f970750f20d58c1b6a25ad8e1d
Jane Tracy Muthoni <muchirijane@outlook.com>
2021-04-19 17:07:55 +0300

    using useState hooks

--- projects/react-basics/src/pages/Home.js
+++ projects/react-basics/src/pages/Home.js
@@ -1,12 +1,22 @@
-import Navbar from "../components/Navbar";
+import {useState} from 'react';
 
 const Home = () => {
   const title = 'Building a blog with React';
+  //let name = 'Tracy'
+  const [name, setName] = useState('Mario');
+  const [age, setAge] = useState(25);
+  const handleClick = () =>{
+    console.log('hello react', )
+    setName('Tracy');
+    setAge(20)
+  }
   return ( 
     <div>
-      <Navbar/>
+      
       <div className="home">
         <h1>{title}</h1>
+        <p onClick={handleClick}>My name is {name} and I am {age}</p>
+        <button onClick={handleClick}>Click Me</button>
       </div>
     </div>
    );
