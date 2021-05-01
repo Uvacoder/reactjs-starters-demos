aef0830b00c28140967b5d7223b5547f1de9ef0e
muchirijane <muchirijane@outlook.com>
2021-05-01 11:49:02 +0300

    done with the first assignment

--- react-learning/react-basics/src/App.js
+++ react-learning/react-basics/src/App.js
@@ -1,36 +1,31 @@
-import {useState} from 'react'
-import Expenses from './components/Expenses/Expenses';
-
-
+import Expenses from './components/Expenses';
 
 function App() {
-  
-  const [expenses, setExpenses ]= useState([
+  const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(),
+      date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 3, 28) },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(),
+      date: new Date(2021, 2, 20),
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date(),
+      date: new Date(2021, 4, 1),
     },
-  ]);
-
+  ];
 
   return (
     <div>
-      <h1>Learning React</h1>
+      <h2>Let's get started!</h2>
       <Expenses expenses={expenses}/>
     </div>
   );
