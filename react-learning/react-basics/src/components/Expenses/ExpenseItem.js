bdfbf04dcb3170419bac6ce77d3d434bde281dfa
muchirijane <muchirijane@outlook.com>
2021-05-03 23:36:22 +0300

    working with state hook

--- react-learning/react-basics/src/components/Expenses/ExpenseItem.js
+++ react-learning/react-basics/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
-const ExpenseItem =(props)=> {
-  return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-    </Card>
-  );
-}
+const ExpenseItem = (props) => {
+	//let title = props.title;
+	const [ title, setTitle ] = useState(props.title);
+	const titleHandler = () => {
+		setTitle('Title changed');
+	};
+	return (
+		<Card className="expense-item">
+			<ExpenseDate date={props.date} />
+			<div className="expense-item__description">
+				<h2>{title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
+			</div>
+			<button onClick={titleHandler}>Change title</button>
+		</Card>
+	);
+};
 
 export default ExpenseItem;
