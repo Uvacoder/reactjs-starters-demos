aa1159eca489b22e16329613d89bd5fbaf8cd4d7
muchirijane <muchirijane@outlook.com>
2021-06-11 09:31:55 +0300

    added the paths to single posts and content

--- next-js/next-contentful-blog/pages/recipes/[slug].js
+++ next-js/next-contentful-blog/pages/recipes/[slug].js
@@ -1,5 +1,7 @@
 import { createClient } from 'contentful';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
+import Image from 'next/image';
 const client = createClient({
 	space: process.env.CONTENTFUL_SPACE_ID,
 	accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
@@ -35,5 +37,49 @@ export async function getStaticProps({ params }) {
 
 export default function RecipeDetails({ recipe }) {
 	console.log(recipe);
-	return <div>Recipe Details</div>;
+	const {featuredImage, title, method, ingredients, cookingTime} = recipe.fields;
+	return <>
+		<div>
+			<Image src={`http:${featuredImage.fields.file.url}`}  height={featuredImage.fields.file.details.image.height}  width={featuredImage.fields.file.details.image.width} alt={featuredImage.fields.description}/>
+		<h2 className="banner">{title}</h2>
+		<div className="info">
+			<p>Take about {cookingTime} mins to cook</p>
+			<h3>Ingredients :</h3>
+			{ingredients.map(ing =>(
+				<span key={ing}>{ing}</span>
+			))}
+		</div>
+		<div className="method">
+			<h3>Method: </h3>
+			<div>{documentToReactComponents(method) }</div>
+		</div>
+		</div>
+		<style jsx>{`
+			h2,h3 {
+          text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+        }
+        .info p {
+          margin: 0;
+        }
+        .info span::after {
+          content: ", ";
+        }
+        .info span:last-child::after {
+          content: ".";
+        }
+
+			`}
+		</style>
+	</>;
 }
